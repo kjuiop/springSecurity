678d648e6bb6eb878613ff2b1e76ad8fa019a28b
kjuiop <copying_y@naver.com>
2020-09-21 10:40:23 +0900

    security test code

---
+++ src/test/java/com/gig/gongmo/WithUser.java
@@ -0,0 +1,11 @@
+package com.gig.gongmo;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "jake", roles = "USER")
+public @interface WithUser {
+}
